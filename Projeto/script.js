631183ca57abad2d9d00417a6733fe2499336e72
victorSA09 <85522484+victorSA09@users.noreply.github.com>
2022-04-23 15:30:52 -0300

    Add files via upload

---
+++ Projeto/script.js
@@ -0,0 +1,13 @@
+
+
+function cadastro() {
+    let para = document.createElement('p');
+    para.textContent = 'Cadastrado!';
+    document.body.appendChild(para);
+   }
+   
+   function forum() {
+    window.open("index.hmtl")
+}
+
+   
